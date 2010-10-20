24fb475bc97fa788e9da8de631de05300bb684c5
moird <dan@moird.com>
2010-10-20 11:12:48 -0600

    Fixed mod registration Lua updated for 4.0

--- xcalc.lua
+++ xcalc.lua
@@ -50,7 +50,7 @@ xcalc.BindingMap = {
 local frame = CreateFrame("Frame")
 
 --Main Initialization
-function xcalc.events:ADDON_LOADED(...)
+function xcalc.events:ADDON_LOADED(arg1, ...)
 	if( arg1 == "xcalc") then
 		--Mod Initialization
 		SlashCmdList["XCALC"] = xcalc.cmdline
